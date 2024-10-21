4dffb9193c74ff990033dbe0c82f3dbcfc9a6bde
n.q.huy <huynq.winwintek@gmail.com>
2024-10-21 10:54:56 +0700

    FEAT: site 08 tab 03

--- src/views/CQItrainingProcess/CreateTrainingProcess.vue
+++ src/views/CQItrainingProcess/CreateTrainingProcess.vue
@@ -33,25 +33,46 @@
                     t("createTrainingProcess.tab2")
                   }}</label>
                 </p>
+                <p class="radio_tab_lg">
+                  <input
+                    type="radio"
+                    id="radio_3"
+                    value="Three"
+                    v-model="picked"
+                  />
+                  <label for="radio_2">{{
+                    t("createTrainingProcess.tab3")
+                  }}</label>
+                </p>
               </div>
             </div>
           </div>
           <div v-if="mode === 'major'">
             <MajorTab1 ref="majorTab1Ref" v-if="picked === 'One'" />
-            <MajorTab2 ref="majorTab2Ref" v-else />
+            <MajorTab2 ref="majorTab2Ref" v-else-if="picked === 'Two'" />
+            <FormAddFile v-else />
           </div>
           <div v-else>
             <GeneralTab1 ref="generalTab1Ref" v-if="picked === 'One'" />
-            <GeneralTab2 ref="generalTab2Ref" v-else />
+            <GeneralTab2 ref="generalTab2Ref" v-else-if="picked === 'Two'" />
+            <FormAddFile v-else />
           </div>
           <div class="btn_area ta_r">
-            <button class="button btn_md btn_secondary" @click="saveTemp">
+            <button 
+              v-if="picked !== 'Three'" class="button btn_md btn_secondary" @click="saveTemp">
               {{ t("common.saveTemp") }}
             </button>
             <button
               v-if="picked === 'One'"
               class="button btn_md btn_primary ml-4"
               @click="handleNext"
+            >
+              {{ t("common.next") }}
+            </button>
+            <button
+              v-else-if="picked === 'Two'"
+              class="button btn_md btn_primary ml-4"
+              @click="handleNextTwo"
             >
               {{ t("common.next") }}
             </button>
@@ -89,7 +110,8 @@ import { ref } from "vue";
 import { getCurrentInstance } from "vue";
 import { EduCourseCqiReq } from "@/stores/cqiTrainingProcess/cqiTrainingProcess.type";
 import { saveEduCourseCqi } from "@/stores/cqiTrainingProcess/cqiTrainingProcess.service";
-import { STS_EDU_CQI_SUCCESS } from "@/constants/common.const";
+import { STS_EDU_CQI_CREATE, STS_EDU_CQI_SUCCESS } from "@/constants/common.const";
+import FormAddFile from "./FormAddFile.vue";
 
 const { t } = useI18n();
 const route = useRoute();
@@ -117,42 +139,65 @@ const state = window.history.state;
 const { deptCd, typeSeq, year } = state;
 
 const saveTemp = () => {
-
-}
+  
+  proxy.$confirm(t("common.message.confirmSaveTemp"), "", (isConfirm: Boolean) => {
+    if (isConfirm) {
+      store.setLoading(true);
+      if (dataOverview.value && dataResult.value) {
+        const dataSave = {
+          eduCursCqiSeq: "",
+          year: year,
+          deptCd: deptCd,
+          eduCursTypeSeq: typeSeq,
+          stsCd: STS_EDU_CQI_CREATE,
+          usagePlan: dataOverview.value.usagePlan,
+          overview: dataOverview.value,
+          evalStnrd: dataResult.value,
+        };
+        saveEduCourseCqi(dataSave)
+          .then((res) => {
+            handleRedirectMenu();
+            proxy.$alert(proxy.$t("common.message.successSaveTemp"));
+          })
+          .finally(() => {
+            store.setLoading(false);
+          });
+      }
+    }
+  });
+};
 
 const saveData = () => {
   if (store.check) {
     proxy.$alert(proxy.$t("common.messageValidateRequired"));
     return;
   }
-  store.setLoading(true);
-  if (mode.value === "major") {
-    if (majorTab2Ref.value) {
-      dataResult.value = majorTab2Ref.value.getData();
+
+  proxy.$confirm(t("common.message.save"), "", (isConfirm: Boolean) => {
+    if (isConfirm) {
+      store.setLoading(true);
+      if (dataOverview.value && dataResult.value) {
+        const dataSave = {
+          eduCursCqiSeq: "",
+          year: year,
+          deptCd: deptCd,
+          eduCursTypeSeq: typeSeq,
+          stsCd: STS_EDU_CQI_SUCCESS,
+          usagePlan: dataOverview.value.usagePlan,
+          overview: dataOverview.value,
+          evalStnrd: dataResult.value,
+        };
+        saveEduCourseCqi(dataSave)
+          .then((res) => {
+            handleRedirectMenu();
+            proxy.$alert(proxy.$t("common.message.saveSuccess"));
+          })
+          .finally(() => {
+            store.setLoading(false);
+          });
+      }
     }
-  } else {
-    if (generalTab2Ref.value) {
-      dataResult.value = generalTab2Ref.value.getData();
-    }
-  }
-  if (dataOverview.value && dataResult.value) {
-    const dataSave = {
-      eduCursCqiSeq: "",
-      year: year,
-      deptCd: deptCd,
-      eduCursTypeSeq: typeSeq,
-      stsCd: STS_EDU_CQI_SUCCESS,
-      usagePlan: dataOverview.value.usagePlan,
-      overview: dataOverview.value,
-      evalStnrd: dataResult.value
-    }
-    saveEduCourseCqi(dataSave).then((res) => {
-      handleRedirectMenu();
-      proxy.$alert(proxy.$t("common.message.saveSuccess"));
-    }).finally(() => {
-      store.setLoading(false);
-    })
-  }
+  });
 };
 
 const handleNext = () => {
@@ -162,15 +207,33 @@ const handleNext = () => {
   }
   if (mode.value === "major") {
     if (majorTab1Ref.value) {
-      dataOverview.value = majorTab1Ref.value.getData()
+      dataOverview.value = majorTab1Ref.value.getData();
     }
   } else {
     if (generalTab1Ref.value) {
-      dataOverview.value = generalTab1Ref.value.getData()
+      dataOverview.value = generalTab1Ref.value.getData();
     }
   }
   window.scrollTo({ top: 0, behavior: "smooth" });
   picked.value = "Two";
+};
+
+const handleNextTwo = () => {
+  if (store.check) {
+    proxy.$alert(proxy.$t("common.messageValidateRequired"));
+    return;
+  }
+  if (mode.value === "major") {
+    if (majorTab2Ref.value) {
+      dataResult.value = majorTab2Ref.value.getData();
+    }
+  } else {
+    if (generalTab2Ref.value) {
+      dataResult.value = generalTab2Ref.value.getData();
+    }
+  }
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  picked.value = "Three";
 };
 
 const handleRedirectMenu = () => {
---
+++ src/views/CQItrainingProcess/FormAddFile.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="section_tit_wrap mt_8">
+    <h4 class="section_tit_xs">파일 첨부하기</h4>
+  </div>
+  <div class="tbl tbl_row">
+    <table>
+      <caption>
+        {{
+          t("majorTab1.table7.caption")
+        }}
+      </caption>
+      <colgroup>
+        <col style="width: 22%" />
+        <col style="width: auto" />
+      </colgroup>
+      <tbody>
+        <tr>
+          <th scope="row">
+            첨부파일
+          </th>
+          <td>
+            <InputFileBase :id="'id2'" :name="'id2'" :type="'office'" :referKey="2" :mode="'create'"
+                :multiple="true" :maxFile="100000" ref="childRef" :orgName="'EDU_COURSE_CQI'"
+                :category="'EDU_COURSE_CQI'"
+                :sectionName="'EDU_COURSE_CQI'">
+            </InputFileBase>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { useI18n } from "vue-i18n";
+import { ref } from "vue";
+import { commonStore } from "@/stores/common";
+
+const { t } = useI18n();
+const store = commonStore();
+
+const state = window.history.state;
+const { deptCd, typeSeq, year, deptNm } = state;
+
+const dataSave = ref({});
+
+
+const getData = () => {
+  return dataSave.value;
+};
+
+defineExpose({
+  getData,
+});
+</script>
+
+<style scoped lang="css">
+.between {
+  justify-content: space-between;
+}
+
+.list_ul {
+  text-align: left;
+}
+
+.list_ul li:before {
+  content: "\2022";
+  position: relative;
+  top: -4px;
+  left: -4px;
+  padding-left: 4px;
+}
+
+.tbl table tbody td,
+.tbl table tbody th {
+  border-left: 1px solid var(--dark1);
+  border-right: 1px solid var(--dark1);
+  vertical-align: middle;
+}
+
+.tbl table thead th {
+  border-left: 1px solid var(--gray-lavender);
+  border-right: 1px solid var(--gray-lavender);
+  border-bottom: 1px solid var(--gray-lavender);
+}
+
+.tbl table thead tr:last-child th,
+.tbl table thead tr th[rowspan] {
+  border-bottom: none !important;
+}
+
+.tbl table thead th:first-child {
+  border-left: 1px solid var(--dark1);
+}
+
+.tbl table thead th:last-child {
+  border-right: 1px solid var(--dark1);
+}
+
+.tbl_row table tbody th {
+  padding: 18px 6px !important;
+  background: var(--dark1);
+  color: var(--white-color);
+}
+.attach_filewrap {
+    padding-left: 0;
+}
+</style>
